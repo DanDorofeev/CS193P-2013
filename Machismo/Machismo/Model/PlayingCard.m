7256db175291f613550710d96b4a79500241fb42
Katie <katie@apples-MacBook-Pro.local>
2013-02-06 15:26:17 -0700

    code added from class Lecture #2
    All the code added to from the lecture 2, I need to do my
    implementation or enhancement.

--- Machismo/Machismo/Model/PlayingCard.m
+++ Machismo/Machismo/Model/PlayingCard.m
@@ -10,24 +10,46 @@
 
 @implementation PlayingCard
 
+@synthesize  suit = _suit; //because we provide setter and getter
+
 - (NSString *)contents
 {
-    NSArray *rankStrings = @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K",];
+    
+    NSArray *rankStrings = [PlayingCard rankStrings];
     return [rankStrings[self.rank] stringByAppendingString:self.suit];
 }
 
++ (NSArray *)validSuits
+{
+    return @[@"♥",@"♦",@"♠",@"♣"];
+}
 
-- (void)setSuit:(NSString *)suit
+
+- (void)setSuit:(NSString *)suit //Setter for suit
 {
     if([@[@"♥",@"♦",@"♠",@"♣"] containsObject:suit]){
-        suit = suit ;
+        _suit = suit ;
     }
 }
 
 
-- (NSString *) suit
+- (NSString *) suit //Getter for suit
 {
     return _suit ? _suit : @"?";
 }
 
++ (NSArray *)rankStrings
+{
+    return @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K",];
+}
+
++ (NSUInteger)maxRank { return [self rankStrings].count -1 ;}
+
+- (void)setRank:(NSUInteger)rank
+{
+    if( rank <= [PlayingCard maxRank]){
+        _rank = rank;
+    }
+        
+}
 @end
